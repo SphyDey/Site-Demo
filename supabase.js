d6a4e6448cb931f65f495858b06cb5b213013d8b
SphyDey <sphydey@gmail.com>
2025-09-21 20:44:10 +0800

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
 // Supabase client (hardcoded - provided by user)
-const SUPABASE_URL = "https://pmvjgkvxdydenykpqfrj.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBtdmpna3Z4ZHlkZW55a3BxZnJqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgyODY0NjksImV4cCI6MjA3Mzg2MjQ2OX0.K0kR01w1xIpkNyilNHrD66Dq3lm1_9X8CdHV0kgXAGU";
+const SUPABASE_URL = "https://ivomuzlgbbnbprkfjtaz.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml2b211emxnYmJuYnBya2ZqdGF6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg0NDU4MzUsImV4cCI6MjA3NDAyMTgzNX0.oPE6UVKpbDDvRb2orQdnRMRnYv-KYz9sn03of2e44K4";
 
 window.supabase = window.supabase || {};
 (function(){
